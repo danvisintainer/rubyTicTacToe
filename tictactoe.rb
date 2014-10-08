16612c4d7f4cb8a3ed75648388e7b644ca9c7e8f
danvisintainer <dan@danvisintainer.com>
2014-10-07 23:39:14 -0400

    Fixed accessing class variables

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,23 +1,20 @@
 class TicTacToe
-	def new
-		board = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-		used = 0
-	end
-
+	@@board = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+	@@used = 0
 
 	def printBoard
-		puts "\n " + board[1] + " | " + board[2] + " | " + board[3]
+		puts "\n " + @@board[1] + " | " + @@board[2] + " | " + @@board[3]
 		puts "---|---|---"
-		puts " " + board[4] + " | " + board[5] + " | " + board[6]
+		puts " " + @@board[4] + " | " + @@board[5] + " | " + @@board[6]
 		puts "---|---|---"
-		puts " " + board[7] + " | " + board[8] + " | " + board[9] + "\n"
+		puts " " + @@board[7] + " | " + @@board[8] + " | " + @@board[9] + "\n"
 	end
 
 	def thereIsAWinner
 		# check for horizontal wins:
 		i = 1
 		while i < 9
-			if board[i] == board[i+1] && board[i+1] == board[i+2]
+			if @@board[i] == @@board[i+1] && @@board[i+1] == @@board[i+2]
 				puts "Horizontal win on row %s" % [i]
 				return true
 			end
@@ -27,7 +24,7 @@ class TicTacToe
 		# check for vertical wins:
 		i = 1
 		while i < 9
-			if board[i] == board[i+3] && board[i+3] == board[i+6]
+			if @@board[i] == @@board[i+3] && @@board[i+3] == @@board[i+6]
 				puts "Vertical win on column %s" % [i]
 				return true
 			end
@@ -35,12 +32,12 @@ class TicTacToe
 		end
 
 		# check for diagonal wins
-		if board[1] == board[5] && board[5] == board[9]
+		if @@board[1] == @@board[5] && @@board[5] == @@board[9]
 			puts "Inverse diagonal win"
 			return true
 		end
 
-		if board[3] == board[5] && board[5] == board[7]
+		if @@board[3] == @@board[5] && @@board[5] == @@board[7]
 			puts "Diagonal win"
 			return true
 		end
@@ -50,7 +47,7 @@ class TicTacToe
 	end
 
 	def isThisBoxAvailable(n)
-		if board[n] == "X" || board[n] == "O"
+		if @@board[n] == "X" || @@board[n] == "O"
 			return true
 		else
 			return false
@@ -59,18 +56,19 @@ class TicTacToe
 end
 
 def playTicTacToe
-	used = 0
+
+	game = TicTacToe.new
 
 	puts "\nLet's play Tic Tac Toe!\nYou will be X, and I will be O."
-	printBoard(board)
+	game.printBoard
 	puts "\nYou go first. Enter the number of the box\nyou want to place your X. Enter 'q' to quit."
 
-	input = gets.chomp
+	#input = gets.chomp
 
-	while input != "q"
-		processInput
-		input = gets.chomp
-	end
+	#while input != "q"
+	#	processInput
+	#	input = gets.chomp
+	#end
 
 	true
 end
