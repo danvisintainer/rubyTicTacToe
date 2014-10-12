a158628aede6327fd19bc5ad3cfeeba89980f187
danvisintainer <dan@danvisintainer.com>
2014-10-12 17:52:47 -0400

    Made the board print bigger and more readable

--- tictactoe.rb
+++ tictactoe.rb
@@ -3,11 +3,17 @@ class TicTacToe
 	@@used = 0
 
 	def printBoard
+		puts "     |     |     "
 		puts "\n " + @@board[1] + " | " + @@board[2] + " | " + @@board[3]
-		puts "---|---|---"
+		puts "     |     |     "
+		puts "-----|-----|-----"
+		puts "     |     |     "
 		puts " " + @@board[4] + " | " + @@board[5] + " | " + @@board[6]
-		puts "---|---|---"
+		puts "     |     |     "
+		puts "-----|-----|-----"
+		puts "     |     |     "
 		puts " " + @@board[7] + " | " + @@board[8] + " | " + @@board[9] + "\n"
+		puts "     |     |     "
 	end
 
 	def thereIsAWinner
